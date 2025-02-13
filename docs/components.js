a55dab9ceee092ae144e1bf6508fc177a2c417c1
DSantosPascual <d.santos.pascual@gmail.com>
2025-02-13 10:31:48 +0100

    fallo en index.js

---
+++ docs/components.js
@@ -0,0 +1,26 @@
+module.exports = {
+    components:{
+        schemas:{
+            User:{
+                type:'object',
+                properties:{
+                    _id:{
+                        type:'objectId',
+                        description:"task identification number",
+                        example:"23454234234xfsd23423df2"
+                    },
+                    title:{
+                        type:'string',
+                        description:"title task",
+                        example:"Aprender swagger"
+                    },
+                    completed:{
+                        type:'boolean',
+                        description:"task completed or not",
+                        example:"false"
+                    },
+                }
+            }
+        }
+    }
+}
--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,10 @@
 const basicInfo = require('./basicInfo');
+const components = require('./components');
+const tasks = require('./tasks');
 
 module.exports = {
-    ...basicInfo
+    ...basicInfo,
+    ...components,
+    ...tasks
 };
+
